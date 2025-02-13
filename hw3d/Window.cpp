dd6d6d6e2e3db46924afe19972e088f698ccdd06
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-02-13 14:09:27 +0300

    Remove handling OnKeyDown Event for repeating key pressing by checking lParam 30th bit

--- hw3d/Window.cpp
+++ hw3d/Window.cpp
@@ -178,7 +178,10 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 		break;
 	/****************** KEYBOARD MESSAGES *******************/
 	case WM_KEYDOWN:
-		kbd.OnKeyPressed(static_cast<unsigned char>(wParam));
+		if (!(lParam & 0x40000000) || kbd.IsAutorepeatEnabled()) // filter autorepeat key event
+		{
+			kbd.OnKeyPressed(static_cast<unsigned char>(wParam));
+		}
 		break;
 	case WM_KEYUP:
 		kbd.OnKeyReleased(static_cast<unsigned char>(wParam));
