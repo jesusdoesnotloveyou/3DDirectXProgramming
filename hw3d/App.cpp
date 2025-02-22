9ad5979681b470127cf1396da1e866a06eb73fed
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-02-22 17:48:54 +0300

    fixed empty window problem

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -80,8 +80,8 @@ void App::DoFrame()
 	}*/
 
 	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
-	wnd.GetGfx().DrawTestTriangle();
 	wnd.GetGfx().ClearBuffer(c, c, 1.0f);
+	wnd.GetGfx().DrawTestTriangle();
 	wnd.GetGfx().EndFrame();
 
 	/*const float t = timer.Peek();
